570fff3809547b718cdcf39e784bf133f4250847
Kyle GANI <kgani@e4r24p1.jnb.42.fr>
2017-05-27 10:10:38 +0200

    game init functionality

--- includes/retro.h
+++ includes/retro.h
@@ -15,4 +15,13 @@
 
 #include <curses.h>
 
+int WIN_KEY_PRESSED;
+int WIN_WIDTH = 90;
+int WIN_HEIGHT = 30;
+int WIN_CURSX = WIN_WIDTH / 2;
+int WIN_CURSY = WIN_HEIGHT / 2;
+int WIN_STARTX = 0;
+int WIN_STARTY = 0;
+WINDOW *GAME_WIN;
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
 #include "../includes/retro.h"
 
+int keyhook(){
+	WIN_KEY_PRESSED = getch();
+	int ret = 0;
+	switch(WIN_KEY_PRESSED){
+		case KEY_UP:
+			if (WIN_CURSY > 1)
+				WIN_CURSY--;
+			return 0;
+		case KEY_DOWN:
+			if (WIN_CURSY < WIN_HEIGHT - 2)
+				WIN_CURSY++;
+			return 0;
+		case KEY_RIGHT:
+			if (WIN_CURSX < WIN_WIDTH - 2)
+				WIN_CURSX++;
+			return 0;
+		case KEY_LEFT:
+			if (WIN_CURSX > 1)
+				WIN_CURSX--;
+			return 0;
+		case ' ':
+			//firebullet function goes here
+			return 0;
+		case 27:
+			ret = 0;
+			return 0;
+	}
+	return ret;
+}
+
+void initGame(){
+	initscr();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	GAME_WIN = newwin(WIN_HEIGHT,WIN_WIDTH,WIN_STARTY, WIN_STARTX);
+	wrefresh(GAME_WIN);
+	move(WIN_CURSY, WIN_CURSX);
+	box(GAME_WIN, 0, 0);
+}
+
 int main()
 {
-    
+	initGame();
+
+	while (1) {
+		move(WIN_CURSY, WIN_CURSX);
+		box(GAME_WIN, 0, 0);
+		wrefresh(GAME_WIN);
+		if (keyhook() == 1)
+			break;
+	}
+	endwin();
+	return 0;
 }
